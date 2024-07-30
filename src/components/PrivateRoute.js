a9eab150e90334d2690e7400970149691f6cacc3
raehan15 <raehannasir@gmail.com>
2024-07-30 11:08:50 +0500

    Second commit

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { Route, Redirect, Navigate } from "react-router-dom";
-
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//   const isAuthenticated = localStorage.getItem("isAuthenticated");
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={(props) =>
-//         isAuthenticated ? (
-//           <Component {...props} />
-//         ) : (
-//           <Navigate to="/admin/login" />
-//         )
-//       }
-//     />
-//   );
-// };
-
 // export default PrivateRoute;
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
